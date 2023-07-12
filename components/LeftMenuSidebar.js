90d6e23b44971b1c7d33fd629aad63e245056169
karimelkomy <karimelkomy@msn.com>
2023-07-13 01:29:44 +0300

    Update left menu sidebar to open inbox

--- components/LeftMenuSidebar.js
+++ components/LeftMenuSidebar.js
@@ -2,6 +2,8 @@ import { sprintf } from "sprintf-js";
 import Actions from "./Actions";
 
 const projectTab = '//ul[@id="projects_list"]//span[text()="%s"]';
+const inboxTab =
+  '//ul[@data-testid="top-sidebar-nav-items"]//span[text()="Inbox"]';
 
 export default class LeftMenuSidebar {
   constructor() {
@@ -16,5 +18,9 @@ export default class LeftMenuSidebar {
 
   async openProject({ projectDetails }) {
     await this.actions.click(sprintf(projectTab, projectDetails.projectName));
+  }
+
+  async openInbox() {
+    await this.actions.click(inboxTab);
   }
 }
